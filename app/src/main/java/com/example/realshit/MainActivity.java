e6fbca8632aeb322eb5b2207e769ae3a7cf2cc0b
Thommimp <Thommimp@gmail.com>
2022-09-30 01:30:09 +0200

    Shutterfree version 1

--- app/src/main/java/com/example/realshit/MainActivity.java
+++ app/src/main/java/com/example/realshit/MainActivity.java
@@ -4,16 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 public class MainActivity extends AppCompatActivity {
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rmain);
+        setContentView(R.layout.activity_main);
 
     }
 }
--- app/src/main/java/com/example/realshit/StartActivity.java
+++ app/src/main/java/com/example/realshit/StartActivity.java
@@ -7,6 +7,9 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class StartActivity extends AppCompatActivity {
 
 
@@ -14,24 +17,18 @@ public class StartActivity extends AppCompatActivity {
     private Button register;
     private Button login;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
-
-        //linearLayout = findViewById(R.id.linear_layout);
         register = findViewById(R.id.register);
         login = findViewById(R.id.login);
 
-        //linearLayout.animate().alpha(0f).setDuration(1);
 
-//        TranslateAnimation animation = new TranslateAnimation(0, 0, 0, -1000);
-//        animation.setDuration(1000);
-//        animation.setFillAfter(false);
-//        animation.setAnimationListener(new MyAnimationlistener());
 
-//        iconImage.setAnimation(animation);
+
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,26 +44,18 @@ public class StartActivity extends AppCompatActivity {
             }
         });
 
+
     }
 
-//    private class MyAnimationlistener implements Animation.AnimationListener {
-//
-//        @Override
-//        public void onAnimationStart(Animation animation) {
-//
-//        }
-//
-//        @Override
-//        public void onAnimationEnd(Animation animation) {
-//            iconImage.clearAnimation();
-//            iconImage.setVisibility(View.INVISIBLE);
-//            linearLayout.animate().alpha(1f).setDuration(1000);
-//
-//        }
-//
-//        @Override
-//        public void onAnimationRepeat(Animation animation) {
-//
-//        }
-//    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        if (FirebaseAuth.getInstance().getCurrentUser() != null) {
+            Intent intent = new Intent(StartActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+            finish();
+        }
     }
+}
